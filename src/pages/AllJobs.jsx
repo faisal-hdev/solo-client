8c05faf647b5b1f7eb5e14a42c11c7f856474674
faisal-hdev <itsfaisalchowdhury@gmail.com>
2024-09-30 01:25:26 +0600

    Pagination functionallity feature are added and pagination button prev & next button emplement also added

--- src/pages/AllJobs.jsx
+++ src/pages/AllJobs.jsx
@@ -7,17 +7,42 @@ import JobCard from "../components/JobCard";
 const AllJobs = () => {
   const axiosSecure = useAxiosSecure();
   const [jobs, setJobs] = useState([]);
+  const [itemsPerPage, setItemsPerPage] = useState(8);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [count, setCount] = useState(0);
+
   useEffect(() => {
     const getData = async () => {
       const { data } = await axiosSecure.get(
-        `${import.meta.env.VITE_API_URL}/jobs`
+        `${
+          import.meta.env.VITE_API_URL
+        }/all-jobs?page=${currentPage}&size=${itemsPerPage}`
       );
       setJobs(data);
     };
     getData();
+  }, [currentPage, itemsPerPage]);
+
+  useEffect(() => {
+    const getCount = async () => {
+      const { data } = await axiosSecure.get(
+        `${import.meta.env.VITE_API_URL}/jobs-count`
+      );
+      setCount(data.count);
+    };
+    getCount();
   }, []);
 
-  const pages = [1, 2, 3];
+  console.log(count);
+
+  const numbersOfPages = Math.ceil(count / itemsPerPage);
+  const pages = [...Array(numbersOfPages).keys()].map((element) => element + 1);
+
+  // Handle pagination button
+  const handlePaginationButton = (value) => {
+    console.log(value);
+    setCurrentPage(value);
+  };
 
   return (
     <div className="container px-6 py-10 mx-auto min-h-[calc(100vh-306px)] flex flex-col justify-between">
@@ -44,7 +69,6 @@ const AllJobs = () => {
                 placeholder="Enter Job Title"
                 aria-label="Enter Job Title"
               />
-
               <button className="px-1 md:px-4 py-3 text-sm font-medium tracking-wider text-gray-100 uppercase transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:bg-gray-600 focus:outline-none">
                 Search
               </button>
@@ -70,8 +94,14 @@ const AllJobs = () => {
         </div>
       </div>
 
+      {/* Pagination Section */}
       <div className="flex justify-center mt-12">
-        <button className="px-4 py-2 mx-1 text-gray-700 disabled:text-gray-500 capitalize bg-gray-200 rounded-md disabled:cursor-not-allowed disabled:hover:bg-gray-200 disabled:hover:text-gray-500 hover:bg-blue-500  hover:text-white">
+        {/* previous Button */}
+        <button
+          disabled={currentPage === 1}
+          onClick={() => handlePaginationButton(currentPage - 1)}
+          className="px-4 py-2 mx-1 text-gray-700 disabled:text-gray-500 capitalize bg-gray-200 rounded-md disabled:cursor-not-allowed disabled:hover:bg-gray-200 disabled:hover:text-gray-500 hover:bg-blue-500  hover:text-white"
+        >
           <div className="flex items-center -mx-1">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -87,24 +117,30 @@ const AllJobs = () => {
                 d="M7 16l-4-4m0 0l4-4m-4 4h18"
               />
             </svg>
-
-            <span className="mx-1">previous</span>
+            <span className="mx-1">Previous</span>
           </div>
         </button>
 
+        {/* Pagination numbers */}
         {pages.map((btnNum) => (
           <button
+            onClick={() => handlePaginationButton(btnNum)}
             key={btnNum}
-            className={`hidden px-4 py-2 mx-1 transition-colors duration-300 transform  rounded-md sm:inline hover:bg-blue-500  hover:text-white`}
+            className={`hidden ${
+              currentPage === btnNum ? "bg-blue-500 text-white" : ""
+            } px-4 py-2 mx-1 transition-colors duration-300 transform  rounded-md sm:inline hover:bg-blue-500  hover:text-white`}
           >
             {btnNum}
           </button>
         ))}
-
-        <button className="px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform bg-gray-200 rounded-md hover:bg-blue-500 disabled:hover:bg-gray-200 disabled:hover:text-gray-500 hover:text-white disabled:cursor-not-allowed disabled:text-gray-500">
+        {/* Next Button */}
+        <button
+          disabled={currentPage === numbersOfPages}
+          onClick={() => handlePaginationButton(currentPage + 1)}
+          className="px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform bg-gray-200 rounded-md hover:bg-blue-500 disabled:hover:bg-gray-200 disabled:hover:text-gray-500 hover:text-white disabled:cursor-not-allowed disabled:text-gray-500"
+        >
           <div className="flex items-center -mx-1">
             <span className="mx-1">Next</span>
-
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="w-6 h-6 mx-1 rtl:-scale-x-100"
